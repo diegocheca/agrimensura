861220ad24c81ae95a6a96d4811c1bf4783ae60e
diegocheca <diegochecarelli@gmail.com>
2021-01-12 18:50:08 -0300

    avance

--- app/Persona.php
+++ app/Persona.php
@@ -3,9 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Persona extends Model
 {
-    
+    use SoftDeletes;
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'dni',
+        'cuit',
+        'email',
+        'empleado_agrimensor',
+        'domicilio'
+    ];
 }
