1be7cafba2d298c22ff4ea3dd47e1000ef67f99a
diegocheca <diegochecarelli@gmail.com>
2021-01-25 17:08:35 -0300

    sumando todo voyager
    luego hay que limpiar las cosas no se usan

---
+++ vendor/tcg/voyager/src/Widgets/ContactsDimmer.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace TCG\Voyager\Widgets;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use App\Contact;
+
+class ContactsDimmer extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = Contact::select("*")->where("estado", "=", "0")->count();
+        $string = "Usted posee ".$count." emails que no han sido respondidos aun"; //trans_choice('voyager::dimmer.post', $count);
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-mail',
+            'title'  => "{$count} {$string}",
+            'text'   => __('voyager::dimmer.post_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => __('Ver Mensajes'),
+                'link' => route('voyager.contacts.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/02.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('Post'));
+    }
+}
